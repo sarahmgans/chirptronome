fabd0a4832daf961c65e2f0424887f97f7b36dbc
sarahmgans <sarah.gans@hotmail.com>
2020-04-28 10:56:30 -0400

    Updates

---
+++ precache-manifest.6797de9fe4cf447a6ab60df0d3bb88e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c1d4c0b80348619b99739806895180",
+    "url": "/sarahGansProjectFive/index.html"
+  },
+  {
+    "revision": "6ef43ae6fdd54738a8cb",
+    "url": "/sarahGansProjectFive/static/css/main.776823ec.chunk.css"
+  },
+  {
+    "revision": "d310ee9eb9f157982ada",
+    "url": "/sarahGansProjectFive/static/js/2.57d50f07.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/sarahGansProjectFive/static/js/2.57d50f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef43ae6fdd54738a8cb",
+    "url": "/sarahGansProjectFive/static/js/main.9aa809f4.chunk.js"
+  },
+  {
+    "revision": "c08b3298b5d1235fa2c0",
+    "url": "/sarahGansProjectFive/static/js/runtime-main.0e39cd9b.js"
+  },
+  {
+    "revision": "f93aa54873860f3a87ba3dfce27d5232",
+    "url": "/sarahGansProjectFive/static/media/chirp1.f93aa548.wav"
+  },
+  {
+    "revision": "4757f7bfcedb1929c800b843d7b87c5f",
+    "url": "/sarahGansProjectFive/static/media/chirp2.4757f7bf.mp3"
+  }
+]);
